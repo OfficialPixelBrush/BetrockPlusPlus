06535b3ec112d898c474757901cb92b01706b870
TVirtmannVisy <23031327+OfficialPixelBrush@users.noreply.github.com>
2026-04-13 10:30:38 +0200

    Changed license-info text

--- src/bpp_shared/numeric_structs.h
+++ src/bpp_shared/numeric_structs.h
@@ -1,26 +1,11 @@
 /*
-    BetrockPlusPlus
-    Copyright (C) 2026  PixelBrushArt and JcbbcEnjoyer
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (c) 2026, Pixel Brush <pixelbrush.dev>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ * 
 */
 
-/*
-    This header file is responsible for all numeric structs,
-    such as Int3 and similar that make handling of the code-base easier.
-*/
-
+#pragma once
 #include <ostream>
 #include <sstream>
 
