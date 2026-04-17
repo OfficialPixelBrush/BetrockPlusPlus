d90005f6079808fe6f765aad86e487a6dd88aee6
Pixel Brush <23031327+OfficialPixelBrush@users.noreply.github.com>
2026-04-17 12:12:47 +0200

    Added very primitive command handling
    - Added the command manager and command class
    - Added command handling to the ChatMessage handler
    - inlined a few math things
    - /help now works fully

---
+++ src/bpp_server/commands/command_manager.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2025-2026, Pixel Brush <pixelbrush.dev>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+*/
+
+#include "command_manager.h"
+#include "command.h"
+
+std::vector<std::unique_ptr<Command>> CommandManager::registeredCommands;
+
+// Register all commands
+void CommandManager::Init() {
+	// Anyone can run these
+	registeredCommands.push_back(std::make_unique<CommandHelp>());
+	/*
+	registeredCommands.push_back(CommandVersion());
+	registeredCommands.push_back(CommandList());
+	registeredCommands.push_back(CommandPose());
+	registeredCommands.push_back(CommandSpawn());
+	// Needs at least creative mode to run
+	registeredCommands.push_back(CommandTeleport());
+	registeredCommands.push_back(CommandGive());
+	registeredCommands.push_back(CommandHealth());
+	// Must be operator
+	registeredCommands.push_back(CommandUptime());
+	registeredCommands.push_back(CommandTime());
+	registeredCommands.push_back(CommandOp());
+	registeredCommands.push_back(CommandDeop());
+	registeredCommands.push_back(CommandWhitelist());
+	registeredCommands.push_back(CommandKick());
+	registeredCommands.push_back(CommandCreative());
+	registeredCommands.push_back(CommandSound());
+	registeredCommands.push_back(CommandKill());
+	registeredCommands.push_back(CommandGamerule());
+	registeredCommands.push_back(CommandSave());
+	registeredCommands.push_back(CommandStop());
+	registeredCommands.push_back(CommandFree());
+	registeredCommands.push_back(CommandLoaded());
+	registeredCommands.push_back(CommandUsage());
+	registeredCommands.push_back(CommandSummon());
+	registeredCommands.push_back(CommandPopulated());
+	registeredCommands.push_back(CommandInterface());
+	registeredCommands.push_back(CommandRegion());
+	registeredCommands.push_back(CommandSeed());
+	registeredCommands.push_back(CommandEntity());
+	registeredCommands.push_back(CommandModified());
+	registeredCommands.push_back(CommandPacket());
+	*/
+	std::cout << "Registered " << registeredCommands.size() << " command(s)!" << "\n";
+}
+
+// Get all registered commands
+const std::vector<std::unique_ptr<Command>> &CommandManager::GetRegisteredCommands() noexcept { return registeredCommands; }
+
+// Parses commands and executes them
+void CommandManager::Parse(std::string &cmd_string, PlayerSession& session) noexcept {
+	// Remove initial /
+	cmd_string = cmd_string.substr(1);
+	// Set these up for command parsing
+	std::string failureReason = "Syntax";
+	std::vector<std::string> command;
+
+	std::string s;
+	std::stringstream ss(cmd_string);
+
+	while (getline(ss, s, ' ')) {
+		// store token string in the vector
+		command.push_back(s);
+	}
+
+	try {
+		// TODO: Make this efficient
+		for (size_t i = 0; i < registeredCommands.size(); i++) {
+			if (registeredCommands[i]->GetLabel() == command[0]) {
+				failureReason = registeredCommands[i]->Execute(command, session);
+				break;
+			}
+		}
+	} catch (const std::exception &e) {
+		//Betrock::Logger::Instance().Info(std::string(e.what()) + std::string(" on /") + cmd_string);
+	}
+
+	Packet::ChatMessage failPkt;
+
+	if (failureReason == "Syntax") {
+		failPkt.message = "§cInvalid Syntax \"" + cmd_string + "\"";
+	} else if (!failureReason.empty()) {
+		failPkt.message = "§c" + failureReason;
+	}
+	failPkt.Serialize(session.stream);
+}
