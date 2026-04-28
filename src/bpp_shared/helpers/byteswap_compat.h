d061f0395510838363e888b3c3e560484c94281b
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-04-28 10:00:56 -0400

    MSVC compatibility

---
+++ src/bpp_shared/helpers/byteswap_compat.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+
+#ifdef _MSC_VER
+#include <cstdlib>
+#define __builtin_bswap16(x) _byteswap_ushort(x)
+#define __builtin_bswap32(x) _byteswap_ulong(x)
+#define __builtin_bswap64(x) _byteswap_uint64(x)
+#endif
