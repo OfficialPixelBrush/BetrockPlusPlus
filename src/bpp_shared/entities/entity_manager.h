7fc6cc549b6172d9c1bf93559fc0b2d4eadb7df6
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-05-12 14:33:07 -0400

    Entity boilerplate

---
+++ src/bpp_shared/entities/entity_manager.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026, Aidan <JcbbcEnjoyer>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+*/
+#pragma once
+#include "entity.h"
+
+// For ticking all entities and keeping track of them in the world
+struct entityManager {
+
+};
