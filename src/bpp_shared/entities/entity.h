92e630e679fe6e72a116394f5a5473a7bcf100cb
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-05-12 14:41:40 -0400

    Update entity.h

--- src/bpp_shared/entities/entity.h
+++ src/bpp_shared/entities/entity.h
@@ -9,5 +9,20 @@
 // Base entity struct
 struct entity {
 	int id = -1; // Entity ID, -1 is an invalid entity
-
+	boolean preventEntitySpawning = false;
+	entity* riddenByEntity;
+	entity* ridingEntity;
+	double prevPosX;
+	double prevPosY;
+	double prevPosZ;
+	double posX;
+	double posY;
+	double posZ;
+	double motionX;
+	double motionY;
+	double motionZ;
+	float rotationYaw;
+	float rotationPitch;
+	float prevRotationYaw;
+	float prevRotationPitch;
 };
