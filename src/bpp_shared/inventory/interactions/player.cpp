46729b95ac38616898538d87192d6f79191decdb
jwaxy <98110672+jwaxy@users.noreply.github.com>
2026-07-12 22:17:07 +0300

    Implement player inventory crafting

--- src/bpp_shared/inventory/interactions/player.cpp
+++ src/bpp_shared/inventory/interactions/player.cpp
@@ -4,15 +4,31 @@
  * SPDX-License-Identifier: AGPL-3.0-only
 */
 #include "player.h"
+#include "../../entities/entity_player.h"
+#include "inventory/interactions/crafting.h"
+#include "inventory/inventories.h"
 
-PlayerInventoryInteraction::PlayerInventoryInteraction(InventoryPlayer* inv)
-    : InventoryInteraction(inv), playerInventory(inv) {}
+PlayerInventoryInteraction::PlayerInventoryInteraction(InventoryPlayer* inv, Runtime& gameRuntime)
+    : CraftingInventoryInteraction(inv, inv, inv, gameRuntime, 2, 2), playerInventory(inv) {}
 
 bool PlayerInventoryInteraction::canExist() {
 	return playerInventory != nullptr;
 }
 
-void PlayerInventoryInteraction::onShiftClick(int slot) {
+void PlayerInventoryInteraction::shiftClickResult() {
+	ItemStack& result = playerInventory->slots[0];
+	if (result.id == ITEM_INVALID)
+		return;
+
+	ItemStack copy = result;
+	if (playerInventory->mergeItemStackInInventory(copy, true, 9, 44)) {
+		finishCraft();
+	} else {
+		playerInventory->slots[0] = copy.count == 0 ? ItemStack{} : copy;
+	}
+}
+
+void PlayerInventoryInteraction::shiftClickOther(int slot) {
 	auto from = playerInventory->getInventoryAreaFromSlot(slot);
 	auto stack = playerInventory->getStackInSlot(slot);
 	if (!stack)
