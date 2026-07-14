ffcfe6192f83b269da1216c1d38c1004fcc99c92
Pixel Brush <23031327+OfficialPixelBrush@users.noreply.github.com>
2026-07-14 12:16:24 +0200

    Replaced the CLI parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  *
 */
-#include "bpp_shared/config/cli_args.h"
 #include "bpp_shared/helpers/java/java_math.h"
 #include "platforms.h"
+#include "version.h"
 #include <atomic>
 #include <csignal>
 #include <numeric_structs.h>
@@ -44,6 +44,30 @@ BOOL WINAPI consoleCtrlHandler(DWORD dwCtrlType) {
 }
 #endif
 
+#include "quick_arg_parser.hpp"
+
+struct Args : MainArguments<Args> {
+	[[maybe_unused]] inline static const std::string version() noexcept {
+		return std::string(PROJECT_FULL_VERSION_LABEL);
+	}
+	uint16_t port = option("port", 'p', "Port the server will run on (25565)") = 25565;
+	int32_t max_players = option("max_players", '\0',
+	                             "Maximum number of players. Anything less than 0 removes the cap (-1)") = -1;
+	bool enable_whitelist = option("whitelist", 'w', "Enables usage of the whitelist") = false;
+	int64_t seed = option("seed", 's', "Overwrites the worlds current seed") = 0;
+	bool disable_portals = option("no_portals", '\0', "Disables Portal-travel") = false;
+	bool force_nether_spawn = option("force_nether_spawn", '\0', "Makes players spawn in the Nether") = false;
+	uint32_t pregen_radius = option("pregen_radius", '\0',
+	                                "Generates chunks around 0,0 until the desired radius is met") = 5;
+	uint32_t chunk_render_radius = option(
+	    "chunk_render_radius", '\0',
+	    "Radius within which chunks are rendered for clients. On Vanilla clients this caps out at about 16 Chunks") = 5;
+	uint32_t chunk_gen_radius = option("chunk_gen_radius", '\0', "Radius within which chunks are generated") = 5;
+	uint32_t chunk_tick_radius = option("chunk_tick_radius", '\0', "Radius within which chunks are randomly ticked") = 5;
+	uint32_t entity_render_radius = option("entity_render_radius", '\0', "Radius within which entities are shown") = 5;
+	uint32_t entity_tick_radius = option("entity_tick_radius", '\0', "Radius within which entities are ticked") = 5;
+};
+
 static void signalHandler(int /*sig*/) {
 	shutdownRequested.store(true);
 }
@@ -54,21 +78,18 @@ static void signalHandler(int /*sig*/) {
 #endif
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+	// Hook up signals
+	std::signal(SIGINT, signalHandler);
+	std::signal(SIGTERM, signalHandler);
+	// Parse CLI Args
+	Args args{ { argc, argv } };
+	// Init the sine table
+	MathHelper::InitSinTable();
+	// We're ready to roll
 	GlobalLogger().info << "Running on " << PLATFORM_NAME << " (" << BUILD_MODE << ", " << ARCH_NAME << ")\n";
 #if defined(_WIN32) || defined(_WIN64)
 	SetConsoleCtrlHandler(consoleCtrlHandler, TRUE);
 #endif
-
-	std::signal(SIGINT, signalHandler);
-	std::signal(SIGTERM, signalHandler);
-
-	// TODO: Implement fully
-	//CommandLineArguments cliargs = ParseArgs(argc, argv);
-
-	MathHelper::InitSinTable();
-
-	// For testing
-	// Utilities::cleanLevel("world");
 
 #ifdef BUILD_SERVER
 	Server serv;
