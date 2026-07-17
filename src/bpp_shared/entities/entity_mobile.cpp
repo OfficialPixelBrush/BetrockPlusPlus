af6d14451286679ff52be53147bf11a5d280fb0f
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-07-17 09:37:24 -0400

    Did some cleaning up

---
+++ src/bpp_shared/entities/entity_mobile.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2026, Aidan <JcbbcEnjoyer>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ */
+#include "entity_mobile.h"
+
---
+++ src/bpp_shared/entities/entity_mobile.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026, Aidan <JcbbcEnjoyer>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ */
+#pragma once
+
+#include "entity.h"
+#include "inventory/item_stack.h"
+#include "logger.h"
+
+struct MobileEntity : public Entity {
+	
+};
