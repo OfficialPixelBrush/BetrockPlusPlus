1ed3338f5733b3eda0c46845430f1986cc28dd98
Pixel Brush <letsplaytvirtmann@gmail.com>
2026-07-30 22:38:44 +0200

    Revert "Some Sonnet 5-suggested optimizations"
    This reverts commit dee0c5bc3c1c54f5c8d5a12788a8621713e3f937.

--- src/bpp_shared/world/generator/shared/cave_gen.h
+++ src/bpp_shared/world/generator/shared/cave_gen.h
@@ -4,14 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  * Based on code by Mojang Studios (2011)
 */
+
 #pragma once
+
 #include "chunk.h"
 #include "constants.h"
 #include "java_random.h"
 #include "numeric_structs.h"
-#include <deque>
-#include <unordered_map>
-#include <vector>
+
 /**
  * @brief Used to carve caves into the world
  *
@@ -19,32 +19,14 @@
 class CaveGenerator {
 private:
 	static constexpr int32_t M_CARVE_EXTENT_LIMIT = 8;
-	static constexpr size_t M_ORIGIN_CACHE_LIMIT = 4096;
-	struct CaveTunnelStep {
-		Vec3 offset;
-		double radiusXz;
-		double radiusY;
-		bool eligible;
-	};
-	struct CaveTunnel {
-		std::vector<CaveTunnelStep> steps;
-		std::vector<CaveTunnel> branches;
-		int32_t startStep = 0;
-		int32_t tunnelLength = 0;
-		float tunnelRadius = 0.0f;
-		bool isShaft = false;
-	};
 	Java::Random rand = Java::Random();
-	std::unordered_map<Int2, std::vector<CaveTunnel>> originCache;
-	std::deque<Int2> originCacheOrder;
-	const std::vector<CaveTunnel>& GetTunnelsForOrigin(Int2 _chunkOffset);
-	std::vector<CaveTunnel> RecordOrigin(Int2 _chunkOffset);
-	CaveTunnel RecordTunnel(Vec3 _offset, float _tunnelRadius, float _carveYaw, float _carvePitch, int32_t _tunnelStep,
-	                        int32_t _tunnelLength, double _verticalScale);
-	void ApplyTunnel(Chunk& _chunk, const CaveTunnel& _tunnel);
 
 public:
 	CaveGenerator(bool _isNetherCave = false) : isNetherCave(_isNetherCave) {}
 	void GenerateCavesForChunk(Chunk& _chunk, int64_t _seed);
+	void GenerateCaves(Chunk& _chunk, Int2 _chunkOffset);
+	void CarveCave(Chunk& _chunk, Vec3 _offset);
+	void CarveCave(Chunk& _chunk, Vec3 _offset, float _tunnelRadius, float _carveYaw, float _carvePitch,
+	               int32_t _tunnelStep, int32_t _tunnelLength, double _verticalScale);
 	bool isNetherCave = false;
 };
