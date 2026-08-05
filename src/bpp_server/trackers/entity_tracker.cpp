157922ba4ee58ba8b414f960d2cdbf8e93cfebdb
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-08-04 20:25:57 -0400

    Update entity_tracker.cpp

--- src/bpp_server/trackers/entity_tracker.cpp
+++ src/bpp_server/trackers/entity_tracker.cpp
@@ -224,7 +224,7 @@ void EntityTracker::SpawnEntityForPlayer(EntityId _playerId, TrackedEntry& _enti
 		if (username.empty()) {
 			// Fallback
 			auto& playerEntity = dynamic_cast<EntityMPPlayer&>(*_entityEntry.entity);
-			username = playerEntity.session ? playerEntity.session->username : "Billy";
+			username = playerEntity.session ? playerEntity.session->username : "Dave";
 		}
 		pkt.username = username;
 		pkt.Serialize(pSession->stream);
