0143938962165e5e5d26bb04e9d48c611a347e82
JcbbcEnjoyer <148708346+JcbbcEnjoyer@users.noreply.github.com>
2026-08-14 08:00:39 -0400

    Fixed windows meltdown

--- include/CrashCatch.hpp
+++ include/CrashCatch.hpp
@@ -20,8 +20,8 @@ License: MIT
 #if defined(_WIN32)
 #define CRASHCATCH_PLATFORM_WINDOWS
 #define WIN32_LEAN_AND_MEAN
-#include <dbgHelp.h>
 #include <windows.h>
+#include <dbgHelp.h>
 #pragma comment(lib, "dbgHelp.lib") //Auto-link debugging support library
 #elif defined(__linux__)
 #define CRASHCATCH_PLATFORM_LINUX
